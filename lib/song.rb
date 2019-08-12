7561996b7d36d7effd87e0be307e8b660542484b
Raul Sposito <raulsposito@gmail.com>
2019-08-12 22:50:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,12 @@ class Song
   @@count = 0 
   
   def initialize(name, artist, genre)
-    @@count += 1 
-    @@genres << genre 
-    @@artists << artist 
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
   end
   
   attr_accessor :name, :artist, :genre
